1d545d35c945b09e1fba9f3fcf2488ca3d706144
shingo.sugure <gonshi555dashiyo@gmail.com>
2022-05-15 17:07:47 +0900

    フォームのフロントエンドバリデーションの実装

--- html/assets/js/script.js
+++ html/assets/js/script.js
@@ -1,15 +1,81 @@
-function showToastInfo() {
-  const TIME = 4000;
-  const toastElm = document.querySelector('.toast.toast--info');
+const breakPoint = 749;
 
-  if (toastElm !== null) {
-    toastElm.classList.add('active');
-    setTimeout(function () {
-      toastElm.classList.remove('active');
-    }, TIME);
+function activateSubmitBtn($form) {
+  const $submitBtn = $form.querySelector('[type="submit"]');
+
+  if ($form.checkValidity()) {
+    $submitBtn.removeAttribute('disabled');
+  } else {
+    $submitBtn.setAttribute('disabled', true);
   }
 }
 
+function validateForm() {
+  const $form = document.querySelector('.form--validate');
+
+  if (!$form) {
+    return;
+  }
+  const $inputs = document.querySelectorAll('.validate');
+
+  for (const $input of $inputs) {
+    $input.addEventListener('input', function (e) {
+      const $target = $input;
+      const $error = $target.nextElementSibling;
+
+      if (!$target.checkValidity()) {
+        if ($target.validity.valueMissing) {
+          $error.textContent = '入力してください！';
+        } else if ($target.validity.tooShort) {
+          $error.textContent = $target.minLength + '文字以上で入力してください！';
+        } else if ($target.validity.tooLong) {
+          $error.textContent = $target.maxLength + '文字以下で入力してください！';
+        } else if ($target.validity.patternMismatch) {
+          if ($target.classList.contains('validate--email')) {
+            $error.textContent = '正しいメールアドレス形式で入力してください！';
+          } else {
+            $error.textContent = '半角英数字で入力してください！';
+          }
+        } else {
+          $error.textContent = '';
+        }
+      } else {
+        if ($target.classList.contains('validate--password-confirm')) {
+          const $password = document.querySelector('.validate--password');
+          const $passwordConfirm = document.querySelector('.validate--password-confirm');
+
+          if ($password.value !== $passwordConfirm.value) {
+            $error.textContent = 'パスワードを一致させてください！';
+          } else {
+            $error.textContent = '';
+          }
+        } else {
+          $error.textContent = '';
+        }
+      }
+
+      activateSubmitBtn($form);
+    });
+  }
+
+  activateSubmitBtn($form);
+}
+validateForm();
+
+/**
+ * Toast
+ */
+function showToastInfo() {
+  const TIME = 4000;
+  const $toastElm = document.querySelector('.toast.toast--info');
+
+  if ($toastElm !== null) {
+    $toastElm.classList.add('active');
+    setTimeout(function () {
+      $toastElm.classList.remove('active');
+    }, TIME);
+  }
+}
 showToastInfo();
 
 const isImage = (fileName) => {
@@ -31,39 +97,39 @@ const isBase64Image = (fileName) => {
  * Mypage Post
  */
 function initPreview() {
-  const preview = document.getElementById('preview');
-  const formImage = document.getElementById('form__image');
-  const formPreview = document.getElementById('form__preview');
-  const deleteBtn = document.getElementById('delete-image');
-  const hiddenImage = document.getElementById('hidden-image');
+  const $preview = document.getElementById('preview');
+  const $formImage = document.getElementById('form__image');
+  const $formPreview = document.getElementById('form__preview');
+  const $deleteBtn = document.getElementById('delete-image');
+  const $hiddenImage = document.getElementById('hidden-image');
 
-  deleteBtn.addEventListener('click', function () {
-    preview.src = '';
-    formPreview.style.display = 'none';
-    formImage.style.display = 'block';
-    preview.classList.remove('active');
-    hiddenImage.value = '';
+  $deleteBtn.addEventListener('click', function () {
+    $preview.src = '';
+    $formPreview.style.display = 'none';
+    $formImage.style.display = 'block';
+    $preview.classList.remove('active');
+    $hiddenImage.value = '';
   });
 
-  if (isImage(preview.src) || (isBase64Image(preview.src) && preview.classList.contains('active'))) {
-    formPreview.style.display = 'block';
-    formImage.style.display = 'none';
+  if (isImage($preview.src) || (isBase64Image($preview.src) && $preview.classList.contains('active'))) {
+    $formPreview.style.display = 'block';
+    $formImage.style.display = 'none';
   } else {
-    formPreview.style.display = 'none';
-    formImage.style.display = 'block';
+    $formPreview.style.display = 'none';
+    $formImage.style.display = 'block';
   }
 }
 
 function createPreview(elm) {
-  const preview = document.getElementById('preview');
+  const $preview = document.getElementById('preview');
   let fileReader = new FileReader();
 
   fileReader.onload = function () {
-    preview.src = fileReader.result;
+    $preview.src = fileReader.result;
   };
   fileReader.readAsDataURL(elm.files[0]);
 
-  preview.classList.add('active');
+  $preview.classList.add('active');
   initPreview();
 }
 
@@ -79,12 +145,14 @@ if (document.getElementById('form--post') !== null) {
  * Hamburger Menu
  */
 const $mypageName = document.getElementById('mypage__name');
-const breakPoint = 749;
-$mypageName.addEventListener('click', function (e) {
-  e.preventDefault();
-  if (window.innerWidth <= breakPoint) {
-    this.nextElementSibling.classList.toggle('active');
-  } else {
-    return false;
-  }
-});
+
+if ($mypageName !== null) {
+  $mypageName.addEventListener('click', function (e) {
+    e.preventDefault();
+    if (window.innerWidth <= breakPoint) {
+      this.nextElementSibling.classList.toggle('active');
+    } else {
+      return false;
+    }
+  });
+}
--- html/views/register.php
+++ html/views/register.php
@@ -22,34 +22,37 @@ function index(): void
   <h1 class="page-title">ユーザー登録</h1>
 
   <div class="register auth">
-    <form action="<?php echo CURRENT_URI; ?>" method="POST" class="form box">
+    <form action="<?php echo CURRENT_URI; ?>" method="POST" class="form box form--validate" novalidate>
       <div class="form__control">
-        <p class="form__label">ユーザー名</p>
+        <p class="form__label">ユーザー名<span class="required">※</span></p>
         <div class="form__content">
-          <input type="text" name="name" placeholder="Posipan" />
+          <input type="text" name="name" placeholder="Posipan" class="validate" required minlength="1" maxlength="15" />
+          <p class="form__error"></p>
         </div>
       </div>
 
       <div class="form__control">
-        <p class="form__label">メールアドレス</p>
+        <p class="form__label">メールアドレス<span class="required">※</span></p>
         <div class="form__content">
-          <input type="email" name="email" placeholder="example@example.com" />
+          <input type="email" name="email" placeholder="example@example.com" class="validate validate--email" required pattern="^[a-zA-Z0-9.!#$&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$" />
+          <p class="form__error"></p>
         </div>
       </div>
 
       <div class="form__control">
-        <p class="form__label">パスワード</p>
+        <p class="form__label">パスワード<span class="required">※</span></p>
         <div class="form__content">
-          <input type="password" name="password" />
+          <input type="password" name="password" class="validate validate--password" required minlength="8" pattern="[a-zA-Z0-9]+" />
+          <p class="form__error"></p>
         </div>
       </div>
 
       <div class="form__control">
-        <label for="password-confirm" class="form__label">パスワード確認</label>
+        <p class="form__label">パスワード確認<span class="required">※</span></p>
         <div class="form__content">
-          <input type="password" name="password-confirm" id="password-confirm" />
+          <input type="password" name="password-confirm" class="validate validate--password-confirm" />
+          <p class="form__error"></p>
         </div>
-        <!-- <p class="form__error">パスワードは8文字以上入力してください。</p> -->
       </div>
 
       <div class="form__btn">
