0217434b528dc4d91829c0eaf9d5b6da48b8180c
shingo.sugure <gonshi555dashiyo@gmail.com>
2022-05-18 08:38:04 +0900

    文字コード設定を編集

--- html/layout/header.php
+++ html/layout/header.php
@@ -27,7 +27,7 @@ function header(): void
   <html lang="ja">
 
   <head>
-    <meta charset="UTF-8">
+    <meta http-equiv="content-type" charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta property="og:site_name" content="" />
