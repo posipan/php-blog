9769ec57bd609a78f04a6b93de3249112f1be935
shingo.sugure <gonshi555dashiyo@gmail.com>
2022-05-17 12:25:10 +0900

    条件文の書き方を変更

--- html/assets/ts/img.ts
+++ html/assets/ts/img.ts
@@ -1,56 +1,72 @@
 /**
  * Thumbnail Image
  */
-const isImage = (fileName: string): boolean => {
-  const image = fileName.substring(fileName.lastIndexOf('.'));
-  if (image.match(/\.(jpg|jpeg|png|gif|svg)$/i)) {
-    return true;
+
+export const setPreview = () => {
+  const $preview = document.getElementById('preview') as HTMLImageElement;
+
+  if (!$preview) {
+    return;
   }
-  return false;
-};
 
-const isBase64Image = (fileName: string): boolean => {
-  if (fileName.match(/^(data:image)?/i)) {
-    return true;
-  }
-  return false;
-};
-
-const $preview = document.getElementById('preview') as HTMLImageElement;
-
-export const initPreview = (): void => {
-  const $formImage = document.getElementById('form__image') as HTMLElement;
-  const $formPreview = document.getElementById('form__preview') as HTMLElement;
-  const $deleteBtn = document.getElementById('delete-image') as HTMLElement;
-  const $hiddenImage = document.getElementById('hidden-image') as HTMLInputElement;
-
-  $deleteBtn.addEventListener('click', function () {
-    $preview.src = '';
-    $formPreview.style.display = 'none';
-    $formImage.style.display = 'block';
-    $preview.classList.remove('active');
-    $hiddenImage.value = '';
-  });
-
-  if (isImage($preview.src) || (isBase64Image($preview.src) && $preview.classList.contains('active'))) {
-    $formPreview.style.display = 'block';
-    $formImage.style.display = 'none';
-  } else {
-    $formPreview.style.display = 'none';
-    $formImage.style.display = 'block';
-  }
-};
-
-export const createPreview = (elem: HTMLInputElement): void => {
-  const fileReader = new FileReader();
-  const file = (elem.files as FileList)[0];
-
-  fileReader.onload = () => {
-    $preview.src = fileReader.result as string;
+  const isImage = (fileName: string): boolean => {
+    const image = fileName.substring(fileName.lastIndexOf('.'));
+    if (image.match(/\.(jpg|jpeg|png|gif|svg)$/i)) {
+      return true;
+    }
+    return false;
   };
 
-  fileReader.readAsDataURL(file);
+  const isBase64Image = (fileName: string): boolean => {
+    if (fileName.match(/^(data:image)?/i)) {
+      return true;
+    }
+    return false;
+  };
 
-  $preview.classList.add('active');
-  initPreview();
+  const initPreview = (): void => {
+    const $formImage = document.getElementById('form__image') as HTMLElement;
+    const $formPreview = document.getElementById('form__preview') as HTMLElement;
+    const $deleteBtn = document.getElementById('delete-image') as HTMLElement;
+    const $hiddenImage = document.getElementById('hidden-image') as HTMLInputElement;
+
+    $deleteBtn.addEventListener('click', function () {
+      $preview.src = '';
+      $formPreview.style.display = 'none';
+      $formImage.style.display = 'block';
+      $preview.classList.remove('active');
+      $hiddenImage.value = '';
+    });
+
+    if (isImage($preview.src) || (isBase64Image($preview.src) && $preview.classList.contains('active'))) {
+      $formPreview.style.display = 'block';
+      $formImage.style.display = 'none';
+    } else {
+      $formPreview.style.display = 'none';
+      $formImage.style.display = 'block';
+    }
+  };
+
+  const createPreview = (elem: HTMLInputElement): void => {
+    const fileReader = new FileReader();
+    const file = (elem.files as FileList)[0];
+
+    fileReader.onload = () => {
+      $preview.src = fileReader.result as string;
+    };
+
+    fileReader.readAsDataURL(file);
+
+    $preview.classList.add('active');
+    initPreview();
+  };
+
+  if (document.querySelector('.form--post')) {
+    initPreview();
+
+    const uploadImage = document.getElementById('upload-image') as HTMLInputElement;
+    uploadImage.addEventListener('change', function () {
+      createPreview(this);
+    });
+  }
 };
--- html/assets/ts/msg.ts
+++ html/assets/ts/msg.ts
@@ -2,7 +2,7 @@ export const showMessage = (elem: string): void => {
   const time: number = 4000;
   const $msgElm = document.querySelector(elem);
 
-  if ($msgElm !== null) {
+  if ($msgElm) {
     $msgElm.classList.add('active');
     setTimeout(() => {
       $msgElm.classList.remove('active');
